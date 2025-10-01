5fd45bce0c7ec652d24fe1ff9805087a2b964bd0
Mike <rans@email.com>
2025-10-01 14:52:50 +1300

    Need to install packages as we go

--- install.sh
+++ install.sh
@@ -4,21 +4,16 @@ pacboy -S binutils --noconfirm
 rm -rf packages
 mkdir packages
 
-package_names=(SDL gnustep-make gnustep-base pcaudiolib espeak-ng nspr spidermonkey)
+package_names=(pcaudiolib espeak-ng nspr spidermonkey gnustep-make gnustep-base SDL)
 
 for packagename in "${package_names[@]}"; do
     echo "Making $packagename package"
 	cd mingw-w64-$packagename
 	makepkg -s -f --noconfirm
+	pacman -U *$packagename*any.pkg.tar.zst --noconfirm
 	rm -f ../packages/*$packagename*any.pkg.tar.zst
 	mv *$packagename*any.pkg.tar.zst ../packages
 	cd ..	
-done
-
-cd packages
-for packagename in "${package_names[@]}"; do
-    echo "Installing $packagename package"
-	pacman -U *$packagename*any.pkg.tar.zst --noconfirm
 done
 
 pacman -Q > installed-packages.txt
